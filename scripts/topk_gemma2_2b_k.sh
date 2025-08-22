27248564d15bbb1d17071ca5703a07579acb41ca
xzascc <xzascc3944@gmail.com>
2025-08-21 23:24:58 -0400

    Feat: update the log path for training with multiple K

--- scripts/topk_gemma2_2b_k.sh
+++ scripts/topk_gemma2_2b_k.sh
@@ -2,6 +2,7 @@ for k in 50 100 150 200 250 300 350 400 450 500; do
 uv run src/Top_AbsoluteK/trainer.py --model_name "google/gemma-2-2b" \
  --model_layer 12 \
  --dataset "pyvene/axbench-concept16k_v2" \
+ --log_path "logs/topk_gemma2_2b_k" \
  --sae_name "topk" \
  --save_steps 2000 \
  --steps 10000 \
